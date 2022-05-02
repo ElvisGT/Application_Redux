5b69b314563042da2e5f45b71f98a19c9a16df4e
Elvis <elvisgiltorres@gmail.com>
2022-05-01 21:05:05 -0400

    Trabajando en la logica del menu al hacer scroll en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 // import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {ShowMenu} from '../actions/actions';
 
 const Header = (props) => {
     const {show_menu} = props;
+    const [menuScroll,setMenuScroll] = useState(false);
+
+    useEffect(() => {
+        window.onscroll = () => {
+            const scroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+            if(scroll > 150){
+                setMenuScroll(true);
+            }else{
+                setMenuScroll(false);
+            }
+        }
+    }, [])
 
     const handleMenu = () => {
         props.ShowMenu();
     }
 
     return (
-        <div className="Header">
+        <div className="Header" id="header">
             <h1 className="Header-title">ElvisDev</h1>
-            {show_menu 
+            {show_menu
                 ? 
                     <div className="Header-menu">
                         <button type="button" 
@@ -32,6 +45,12 @@ const Header = (props) => {
                             <img />
                     </button>
             }
+
+            {menuScroll && 
+                <div className="Header-menu-scroll">Menu Scroll</div>
+            }
+
+           
         </div>
     )
 }
