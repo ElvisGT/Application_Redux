040b73c7eb2b91475ec0e73a08b6a8ca8ce45993
Elvis <elvisgiltorres@gmail.com>
2022-05-02 21:14:26 -0400

    Estilando el home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,17 +4,15 @@ import {Link} from 'react-router-dom';
 const Home = () => {
     return (
         <div className="Home">
-            <img src="" alt="" />
-                <div>
-                     <p>
+            <img className="Home-img" src="" alt="" />
+                <div className="Home-information">
+                     <p className="Home-information-p">
                         Bienvenid@ a mi sitio web donde la temática principal
                         son los videojuegos. Cuenta con una sección de buscador
                         donde puede hacer su busqueda por el nombre del juego que 
-                        desea y se mostrará toda la información 
-                         
-                         
+                        desea y se mostrará toda la información disponible.
                     </p> 
-                     <Link to="/buscador">Ir al Buscador</Link> 
+                     <Link className="Home-information-link" to="/buscador">Ir al Buscador</Link> 
                 </div>
         </div>
     )
