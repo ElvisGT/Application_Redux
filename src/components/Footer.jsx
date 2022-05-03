25955b88840077efe1bbdaee81cea8268efa1b1a
Elvis <elvisgiltorres@gmail.com>
2022-05-02 21:36:21 -0400

    Estilando el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className="Footer">
-            <h1>Footer</h1>
+            <a className="Footer-vinculo" href="/">LinkedIn</a>
+            <a className="Footer-vinculo" href="/">Github</a>
+            <a className="Footer-vinculo" href="https://elvisgt.github.io/portfolio">Portfolio</a>
+
+            <p className="Footer-right">Todos los derechos reservados.</p>
         </div>
     )
 
