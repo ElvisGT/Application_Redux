ab11f10addc2a22d138a672a77a568c8649e55ca
Elvis <elvisgiltorres@gmail.com>
2022-05-02 20:42:33 -0400

    Trabajando en la maquetacion del home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
     return (
-        <h1>Hola</h1>
+        <div className="Home">
+            <img src="" alt="" />
+                <div>
+                     <p>
+                        Bienvenid@ a mi sitio web donde la temática principal
+                        son los videojuegos. Cuenta con una sección de buscador
+                        donde puede hacer su busqueda por el nombre del juego que 
+                        desea y se mostrará toda la información 
+                         
+                         
+                    </p> 
+                     <Link to="/buscador">Ir al Buscador</Link> 
+                </div>
+        </div>
     )
 }
 
