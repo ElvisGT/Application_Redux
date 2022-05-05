557ed4df39ba9b20239a96590974e895e3e303c2
Elvis <elvisgiltorres@gmail.com>
2022-05-05 15:06:37 -0400

    Agregando las redes sociales al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className="Footer">
-            <a className="Footer-vinculo" href="/">LinkedIn</a>
-            <a className="Footer-vinculo" href="/">Github</a>
+            <a className="Footer-vinculo" href="https://www.linkedin.com/in/elvis-gil-torres-ab64a9225/">LinkedIn</a>
+            <a className="Footer-vinculo" href="https://github.com/ElvisGT">Github</a>
             <a className="Footer-vinculo" href="https://elvisgt.github.io/portfolio">Portfolio</a>
 
             <p className="Footer-right">Todos los derechos reservados.</p>
